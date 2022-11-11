ccc882f38f970a9baee806170bee70284d593190
David Pitta <davidfrpitta@gmail.com>
2022-11-11 20:56:29 -0300

    adicionando arquivos principais

---
+++ Usuario.py
@@ -0,0 +1,23 @@
+from util import bd
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class Usuario():
+    def cadastro(self, nm, cd, cpf, email, senha, tel, dt_nasc):
+        banco = bd.SQL()
+        comando = '''
+                  INSERT INTO tb_usuario (nm_usuario, cd_sexo, cpf_usuario, email_usuario, senha_usuario, tel_usuario, dt_nasc_usuario)
+                  VALUES (%s, %s, %s, %s, %s, %s, %s)
+                  '''
+        banco.executar(comando, [nm, cd, cpf, email, generate_password_hash(senha), tel, dt_nasc])
+
+    def login(self, e, s):
+        banco = bd.SQL()
+        comando = "SELECT email_usuario, senha_usuario FROM tb_usuario where email_usuario = %s"
+        cs = banco.consultar(comando, [e])
+        if cs:
+            for [email, senha] in cs:
+                if email == e:
+                    if check_password_hash(senha, s):
+                      return True
+
+        return False
---
+++ util/bd.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+class SQL:
+   def __init__(self):
+       self.cnx = mysql.connector.connect(user="root", password="David21*",\
+                                          host='127.0.0.1',\
+                                          database="db_loja")
+
+   def executar(self, comando, parametros):
+       cs = self.cnx.cursor()
+       cs.execute(comando, parametros)
+       self.cnx.commit()
+       cs.close()
+       return True
+
+   def consultar(self, comando, parametros):
+       cs = self.cnx.cursor()
+       cs.execute(comando, parametros)
+       return cs
+
+   def __del__(self):
+       self.cnx.close()
