07df769446fe8af4c5eed4bcb5f68a7a404c7dec
David Pitta <davidfrpitta@gmail.com>
2022-11-12 23:55:28 -0300

    adicionando espaços

--- principal.py
+++ principal.py
@@ -7,12 +7,14 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 @app.route('/')
 def index():
     if not session.get('email'):
         return redirect('/login')
 
     return render_template('index.html')
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -38,8 +40,10 @@ def logout():
     session['email'] = None
     return redirect('/')
 
+
 @app.route('/esqueciSenha')
 def esqueciSenha():
     return render_template('esqueciSenha.html')
+
 
 app.run(debug=True)
