4921e6112a79f18f44951d62f0b5e16621b9b4c7
David Pitta <davidfrpitta@gmail.com>
2022-11-17 10:39:11 -0300

    Alterando gitignore e usando o .env

--- util/bd.py
+++ util/bd.py
@@ -1,20 +1,27 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 class SQL:
-   def __init__(self):
-       self.cnx = mysql.connector.connect(user="root", password="12345678", host='127.0.0.1', database="db_loja")
+    def __init__(self):
+        user = os.environ.get('user')
+        password = os.environ.get('password')
+        host = os.environ.get('host')
+        database = os.environ.get('database')
+        self.cnx = mysql.connector.connect(user=user, password=password, host=host, database=database)
 
-   def executar(self, comando, parametros):
-       cs = self.cnx.cursor()
-       cs.execute(comando, parametros)
-       self.cnx.commit()
-       cs.close()
-       return True
+    def executar(self, comando, parametros):
+        cs = self.cnx.cursor()
+        cs.execute(comando, parametros)
+        self.cnx.commit()
+        cs.close()
+        return True
 
-   def consultar(self, comando, parametros):
-       cs = self.cnx.cursor()
-       cs.execute(comando, parametros)
-       return cs
+    def consultar(self, comando, parametros):
+        cs = self.cnx.cursor()
+        cs.execute(comando, parametros)
+        return cs
 
-   def __del__(self):
-       self.cnx.close()
+    def __del__(self):
+        self.cnx.close()
