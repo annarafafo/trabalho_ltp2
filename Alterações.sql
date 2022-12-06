c7f36ee78af36dd6808c7665866e9df01c563e72
David Pitta <davidfrpitta@gmail.com>
2022-12-06 17:53:10 -0300

    Inserindo forma de pagamento

---
+++ Alterações.sql
@@ -0,0 +1,3 @@
+INSERT INTO `db_loja`.`tb_forma_de_pagamento` (`id_forma_de_pagamento`, `nm_forma_de_pagamento`) VALUES ('1', 'Boleto');
+INSERT INTO `db_loja`.`tb_forma_de_pagamento` (`id_forma_de_pagamento`, `nm_forma_de_pagamento`) VALUES ('2', 'Pix');
+INSERT INTO `db_loja`.`tb_forma_de_pagamento` (`id_forma_de_pagamento`, `nm_forma_de_pagamento`) VALUES ('3', 'Cartão de Crédito');
