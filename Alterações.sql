704a92af7d786a9a54794be707d95ad2eac134bb
David Pitta <davidfrpitta@gmail.com>
2022-12-06 17:58:31 -0300

    Formas de pagamento

--- Alterações.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `db_loja`.`tb_forma_de_pagamento` (`id_forma_de_pagamento`, `nm_forma_de_pagamento`) VALUES ('1', 'Boleto');
-INSERT INTO `db_loja`.`tb_forma_de_pagamento` (`id_forma_de_pagamento`, `nm_forma_de_pagamento`) VALUES ('2', 'Pix');
-INSERT INTO `db_loja`.`tb_forma_de_pagamento` (`id_forma_de_pagamento`, `nm_forma_de_pagamento`) VALUES ('3', 'Cartão de Crédito');
--- comandos gerais sql.sql
+++ comandos gerais sql.sql
@@ -5904,3 +5904,7 @@ BEGIN
 	end if;
 END $$
 DELIMITER ;
+
+INSERT INTO `db_loja`.`tb_forma_de_pagamento` (`id_forma_de_pagamento`, `nm_forma_de_pagamento`) VALUES ('1', 'Boleto');
+INSERT INTO `db_loja`.`tb_forma_de_pagamento` (`id_forma_de_pagamento`, `nm_forma_de_pagamento`) VALUES ('2', 'Pix');
+INSERT INTO `db_loja`.`tb_forma_de_pagamento` (`id_forma_de_pagamento`, `nm_forma_de_pagamento`) VALUES ('3', 'Cartão de Crédito');
